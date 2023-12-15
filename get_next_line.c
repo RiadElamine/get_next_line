226097da4d9bb53a740dacdb861ce9f3d8fde111
RiadElamine <139612639+RiadElamine@users.noreply.github.com>
2023-12-16 00:02:18 +0100

    test 5

--- get_next_line.c
+++ get_next_line.c
@@ -6,103 +6,105 @@
 /*   By: relamine <relamine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 22:15:45 by relamine          #+#    #+#             */
-/*   Updated: 2023/12/15 00:30:33 by relamine         ###   ########.fr       */
+/*   Updated: 2023/12/15 23:44:18 by relamine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "get_next_line.h"
 
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 4
+#endif
 
-#define buffer_size 10
 
-// void f()
-// {
-//   system("leaks a.out");
-// }
+void f()
+{
+  system("leaks a.out");
+}
 
 char *get_next_line(int fd) {
-  int count = 0;
-  int index = 1;
+  int check = 0;
+  int index = 0;
+  static char *overneed = NULL;
+  // static int count = 0;
   char *arr;
-  char *q = NULL;
+  char *rs = NULL;
   char *tmp = NULL;
 
 
 
-  arr = malloc(buffer_size + 1);
+  arr = malloc(BUFFER_SIZE + 1);
   ssize_t byte_reading;
 
-  while (index <= buffer_size)
+  while (index < BUFFER_SIZE)
   {
-    byte_reading = read(fd, &arr[index - 1], 1);
+    byte_reading = read(fd, arr, BUFFER_SIZE);
     if (byte_reading == -1)
     {
       perror ("Error in reading file");
       return (NULL);
     }
     else if (byte_reading == 0)
-    {
       return (NULL);
-    }
 
-    if (arr[index - 1] == '\n')
+    char *chrs = ft_strchr(arr, '\n');
+    if (chrs)
     {
-      arr[index] = '\0';
-      tmp = q;
-      q = ft_strjoin(tmp,arr);
+      index = (int)(chrs - arr);
+      overneed = ft_substr(arr,index + 1, BUFFER_SIZE);
+      arr[index + 1] = '\0';
+      tmp = rs;
+      rs = ft_strjoin(tmp,arr);
       free(arr);
       if (tmp)
-      {
         free(tmp);
-      }
-      index++;
       break;
     }
-
-    index++;
-
-    if (index > buffer_size)
+    
+    index = BUFFER_SIZE;
+    //this for complete process of reading line
+    if (index == BUFFER_SIZE)
     {
-      arr[buffer_size] = '\0';
-      if (count == 0)
+      arr[BUFFER_SIZE] = '\0';
+      if (check == 0)
       {
-        q = ft_strjoin(arr, NULL);
-        count++;
+        rs = ft_strjoin(arr, NULL);
+        check = 1;
       }
       else
       {
-        tmp = q;
-        q = ft_strjoin(tmp,arr);
+        tmp = rs;
+        rs = ft_strjoin(tmp,arr);
         free(tmp);
-        count++;
       }
-      index = 1;
+      //initialisation
+      index = 0;
       free(arr);
-      arr = malloc(buffer_size + 1);
+      arr = malloc(BUFFER_SIZE + 1);
     }
   }
-  return (q);
+  return (rs);
 }
 
 
 
 
-// int main()
-//  {
+int main()
+ {
   
-//   int fd = open("file.txt", O_RDONLY);
-//   if (fd == -1)
-//   {
-//     perror ("Error in opening file");
-//     return -1;
-//   }
+  int fd = open("file.txt", O_RDONLY);
+  if (fd == -1)
+  {
+    perror ("Error in opening file");
+    return -1;
+  }
   
-//   printf("%s",get_next_line(fd));
+  printf("%s",get_next_line(fd));
+  printf("%s",get_next_line(fd));
+  // get_next_line(fd);
 
-
-
-//   close(fd);
-//   atexit(f);
-//   return (0);
-//  }
+  close(fd);
+  atexit(f);
+  return (0);
+ }
