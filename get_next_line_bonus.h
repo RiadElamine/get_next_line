a2715f35588c0e256c0c07417049c04f1564096d
RiadElamine <139612639+RiadElamine@users.noreply.github.com>
2024-01-19 22:55:44 +0100

    Update get_next_line_bonus.h

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -11,22 +11,20 @@
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
-#define GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 10
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-#include <limits.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
 
-char    *get_next_line(int fd);
+char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
-int     ft_strchr(char *s, int c);
+int		ft_strchr(char *s, int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strdup(const char *s1);
-size_t	ft_strlen(const char *s);
 
 #endif
