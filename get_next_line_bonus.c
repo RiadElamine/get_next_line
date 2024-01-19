79d3ebb99a162a159c1c8252862d062b230200a0
RiadElamine <139612639+RiadElamine@users.noreply.github.com>
2024-01-19 22:55:05 +0100

    Update get_next_line_bonus.c

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -12,70 +12,66 @@
 
 #include "get_next_line_bonus.h"
 
-static char    *ft_line_get(char **buffer)
+static char	*ft_line_get(char **buffer)
 {
-    char *line;
-    int  findnl;
-    char *tmp;
-    size_t len;
+	char	*line;
+	int		findnl;
+	char	*tmp;
 
-    findnl = ft_strchr(*buffer, '\n');
-    if (findnl == -1)
-    {
-        line = ft_strdup(*buffer);
-        if(line)
-            free(*buffer);
-        return (*buffer = NULL, line);
-    }
-    line = ft_substr(*buffer, 0, findnl + 1);
-    if (!line)
-        return (free(*buffer), *buffer = NULL, NULL);
-    tmp = *buffer;
-    len = ft_strlen(&(tmp[findnl + 1])); 
-    if (len == 0)
-        return (free(tmp), *buffer = NULL, line);
-    *buffer = ft_substr(tmp, findnl + 1, len);
-    return (free(tmp), line); 
-}
-static char    *ft_reading(char *buffer, int fd)
-{
-    ssize_t byte;
-    char *arr;
-
-    arr = malloc((size_t)BUFFER_SIZE + 1);
-    if (!arr)
-        return (free(buffer), NULL);
-    byte = read(fd, arr, BUFFER_SIZE);
-    if (byte == -1) 
-        return (free(arr), free(buffer), NULL);
-    while (byte > 0)
-    {
-        arr[byte] = '\0';
-        buffer = ft_strjoin(buffer, arr);
-        if (!buffer)
-            return (NULL);
-        if  (ft_strchr(arr, '\n') != -1)
-            break;
-        byte = read(fd, arr, BUFFER_SIZE);
-        if (byte == -1)
-            return (free(arr), NULL);
-    }
-    return (free(arr), buffer);
-}
-char    *get_next_line(int fd)
-{
-
-    static char *buffer[OPEN_MAX];
-
-    if ((fd >= 0 && fd <= OPEN_MAX ) && BUFFER_SIZE >= 0)
-    {
-        buffer[fd] = ft_reading(buffer[fd], fd);
-        if  (buffer[fd])
-            return (ft_line_get(&buffer[fd]));
-    }
-    return (NULL);
+	findnl = ft_strchr(*buffer, '\n');
+	if (findnl == -1)
+	{
+		line = ft_strdup(*buffer);
+		if (line)
+			free(*buffer);
+		return (*buffer = NULL, line);
+	}
+	line = ft_substr(*buffer, 0, findnl + 1);
+	if (!line)
+		return (free(*buffer), *buffer = NULL, NULL);
+	tmp = *buffer;
+	if (tmp[findnl + 1] == '\0')
+		return (free(tmp), *buffer = NULL, line);
+	*buffer = ft_substr(tmp, findnl + 1, INT_MAX);
+	return (free(tmp), line);
 }
 
+static char	*ft_reading(char *buffer, int fd)
+{
+	ssize_t	byte;
+	char	*arr;
+
+	arr = malloc((size_t)BUFFER_SIZE + 1);
+	if (!arr)
+		return (free(buffer), NULL);
+	byte = read(fd, arr, BUFFER_SIZE);
+	while (byte > 0)
+	{
+		arr[byte] = '\0';
+		buffer = ft_strjoin(buffer, arr);
+		if (!buffer)
+			return (NULL);
+		if (ft_strchr(arr, '\n') != -1)
+			break ;
+		byte = read(fd, arr, BUFFER_SIZE);
+	}
+	if (byte == -1)
+		return (free(arr), free(buffer), NULL);
+	return (free(arr), buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer[OPEN_MAX];
+
+	if ((fd >= 0 && fd < OPEN_MAX) && (BUFFER_SIZE > 0 && BUFFER_SIZE <= INT_MAX))
+	{
+		buffer[fd] = ft_reading(buffer[fd], fd);
+		if (buffer[fd])
+			return (ft_line_get(&buffer[fd]));
+	}
+	return (NULL);
+}
 
 // void f()
 // {
